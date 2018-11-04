7019d341d63a4390d30fd0bab813f5d98d6265dd
youzi <youzi@DESKTOP-IUGK9CN>
2018-11-04 21:18:48 +0800

    第二次提交

---
+++ web/src/main/java/com/hmxy/web/MainApplication.java
@@ -0,0 +1,13 @@
+package com.hmxy.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
